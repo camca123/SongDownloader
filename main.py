b889f55a5be6aaffc3c22f9160fe3ea287ccc970
camca123 <camca96@gmail.com>
2014-08-25 02:30:35 -0300

    Added main commands

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ import time
 DEVELOPER_KEY = "AIzaSyDw7nsMtFcYUDylgGC2dbu4UHqQy8q-avI"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
+
+
+youtube_search("google", 1)
+
+print "\n".join(search_videos), "\n"
+
+print "\n".join(video_ids), "\n"
+print "\n".join(video_lengths), "\n"
