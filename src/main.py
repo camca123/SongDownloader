5739d4f337c74965d363a2eedc62afc4c633eed7
camca123 <camca96@gmail.com>
2014-08-25 04:34:44 -0300

    Changed stuff

--- src/main.py
+++ src/main.py
@@ -1,14 +1,38 @@
-from apiclient.discovery import build
-import time
+import pafyDemVids
 
-DEVELOPER_KEY = "AIzaSyDw7nsMtFcYUDylgGC2dbu4UHqQy8q-avI"
-YOUTUBE_API_SERVICE_NAME = "youtube"
-YOUTUBE_API_VERSION = "v3"
+vid_duration_list = []
+vid_duration_secs = []
 
+vid_duration_list = pafyDemVids.pafyAllTheVids(raw_input('Enter a song to search: '), raw_input('Max Results(~10?): '))
 
-youtube_search("google", 1)
+for videos in vid_duration_list:
+    hrs = ''
+    mins = ''
+    secs = ''
+    c = 0
+    while c < len(videos.duration):
+        if (videos.duration[c] != ':'):
+            hrs += videos.duration[c]
+        else:
+            c+=1
+            break
+        c+=1
+    while c < len(videos.duration):
+        if (videos.duration[c] != ':'):
+            mins += videos.duration[c]
+        else:
+            c+=1
+            break
+        c+=1
+    while c < len(videos.duration):
+        if (videos.duration[c] != ':'):
+            secs += videos.duration[c]
+        else:
+            c+=1
+            break
+        c+=1
+    vid_duration_secs.append((int(hrs)*60*60)+(int(mins)*60)+int(secs))
+    
+vid_duration_secs.sort()
 
-print "\n".join(search_videos), "\n"
-
-print "\n".join(video_ids), "\n"
-print "\n".join(video_lengths), "\n"
+print vid_duration_secs, "\n"
---
+++ src/pafyDemVids.py
@@ -0,0 +1,27 @@
+from apiclient.discovery import build
+import pafy
+
+DEVELOPER_KEY = "AIzaSyDw7nsMtFcYUDylgGC2dbu4UHqQy8q-avI"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+search_videos = []
+vid_url_list = []
+pafy_vid_list = []
+
+def pafyAllTheVids(query, max_results):
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,developerKey=DEVELOPER_KEY)
+
+    search_response = youtube.search().list(
+        type = 'video',
+        q = query,
+        part = "id,snippet",
+        maxResults = max_results
+        ).execute()
+
+    for search_result in search_response.get("items", []):
+        search_videos.append(search_result["snippet"]["title"])
+        vid_url_list.append("https://www.youtube.com/watch?v="+search_result["id"]["videoId"])
+        video = pafy.new(vid_url_list[-1])
+        pafy_vid_list.append(video)
+    return pafy_vid_list
